4692f8baf546f313cbe26ca2727be43136beb319
kirill matveev <dejar@protonmail.com>
2023-11-30 16:10:21 +0300

    feat: add login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,38 @@
 import styles from './Login.module.scss'
+import Input from 'components/Input/Input'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
 
 function Login() {
-  return <div>Login</div>
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const data = new FormData(e.currentTarget)
+    console.log({
+      login: data.get('login'),
+      password: data.get('password'),
+    })
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Sign In</h1>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <FormControl>
+          <Input name="login" type="text" label="Login" />
+        </FormControl>
+        <Input name="password" type="password" label="Password" />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+      </Box>
+    </div>
+  )
 }
 
 export default Login
