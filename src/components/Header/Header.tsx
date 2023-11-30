a0ed4719071a5d95d0074361cd3752b69b15a4c8
kirill matveev <dejar@protonmail.com>
2023-11-30 16:11:15 +0300

    chore: switch login button to mui component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import styles from './Header.module.scss'
 import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button'
 
 function Header() {
   return (
     <header className={styles.body}>
       <div>Logo</div>
-      <Link to={'/login'}>Login</Link>
+      {/* <Link to={'/login'}>Login</Link> */}
+      <Button component={Link} to="/login">
+        Login
+      </Button>
     </header>
   )
 }
